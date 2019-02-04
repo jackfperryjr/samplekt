dd2e6fc80290a43630a77813eae903132071e71c
jackfperryjr <jack.franklin.perryjr@gmail.com>
2019-02-03 19:22:02 -0500

    Added function to Toast name from user input

--- app/src/main/java/com/jackperryjr/samplekt/MainActivity.kt
+++ app/src/main/java/com/jackperryjr/samplekt/MainActivity.kt
@@ -2,11 +2,22 @@ package com.jackperryjr.samplekt
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton?.setOnClickListener { showText() }
+    }
+
+    private fun showText() {
+        val name = findViewById<EditText>(R.id.mainEditText)
+        if (name != null) {
+            Toast.makeText(this, name.text, Toast.LENGTH_LONG).show()
+        }
     }
 }
